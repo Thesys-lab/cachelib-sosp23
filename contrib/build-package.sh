be1ffca3b653fffbda0201019a7577e24d8b6d28
Jason <peter.waynechina@gmail.com>
2023-02-18 20:59:34 -0500

    fix a compilation issue

--- contrib/build-package.sh
+++ contrib/build-package.sh
@@ -160,6 +160,7 @@ case "$1" in
     REPODIR=cachelib/external/$NAME
     SRCDIR=$REPODIR
     external_git_clone=yes
+    external_git_tag="9.1.0"
     cmake_custom_params="-DBUILD_SHARED_LIBS=ON"
     if test "$build_tests" = "yes" ; then
         cmake_custom_params="$cmake_custom_params -DFMT_TEST=YES"
